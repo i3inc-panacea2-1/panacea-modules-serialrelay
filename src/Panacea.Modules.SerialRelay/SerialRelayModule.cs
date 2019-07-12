d9bc2913fa176d77dde4b1df4ab11dbe258efb90
Kostas Aronis <k.aronis@dotbydot.gr>
2019-07-12 18:10:59 +0300

    Serial relay plugin with hardcoded values

---
+++ src/Panacea.Modules.SerialRelay/SerialRelayModule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Panacea.Modularity.Relays;
+using System.IO.Ports;
+using System.Management;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Panacea.Modules.SerialRelay
+{
+    class SerialRelayModule : IRelayModule
+    {
+        private string portName;
+        const string DEVICE_NAME = "USB-SERIAL CH340 ";
+        const string ON  = "A00101A2";
+        const string OFF = "A00100A1";
+        public SerialRelayModule()
+        {
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_PnPEntity"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                var dev = ports.FirstOrDefault(p => p["Name"]?.ToString().StartsWith(DEVICE_NAME) == true);
+                if(dev!= null)
+                {
+                    Debug.WriteLine(dev["Name"]);
+                    portName = dev["Name"].ToString().Substring(DEVICE_NAME.Length + 1, dev["Name"].ToString().Length - DEVICE_NAME.Length - 2);
+                }
+            }
+        }
+        public Task<bool> SetStatusAsync(bool on, int port)
+        {
+            using (var sp = new SerialPort(portName))
+            {
+                if (sp == null)
+                {
+                    return Task.FromResult(false);
+                }
+                sp.Open();
+                var hex = on ? ON : OFF;
+                var bytes = StringToByteArray(hex);
+                sp.Write(bytes, 0, bytes.Length);
+                sp.Close();
+                return Task.FromResult(true);
+            }
+        }
+
+        private byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
---
+++ src/Panacea.Modules.SerialRelay/SerialRelayPlugin.cs
@@ -0,0 +1,42 @@
+﻿using Panacea.Modularity.Relays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modules.SerialRelay
+{
+    class SerialRelayPlugin : IRelayModulePlugin
+    {
+        IRelayModule _module;
+
+        public Task BeginInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Task EndInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<IRelayModule> GetModuleAsync()
+        {
+            if (_module == null)
+            {
+                _module = new SerialRelayModule();
+            }
+            return Task.FromResult(_module);
+        }
+
+        public Task Shutdown()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
